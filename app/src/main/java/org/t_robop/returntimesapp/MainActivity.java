c2b3a0267e869445f9428eb0cb40513773cabd41
Dai1678 <dai7774@gmail.com>
2016-12-12 17:49:22 +0900

    メール機能、宛先直指定で実装

--- app/src/main/java/org/t_robop/returntimesapp/MainActivity.java
+++ app/src/main/java/org/t_robop/returntimesapp/MainActivity.java
@@ -1,18 +1,14 @@
 package org.t_robop.returntimesapp;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.StrictMode;
-import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,34 +16,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.cast.framework.Session;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Properties;
 
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimeUtility;
 
 
 public class MainActivity extends AppCompatActivity implements GeoTask.Geo, LocationListener {
@@ -123,9 +96,8 @@ public class MainActivity extends AppCompatActivity implements GeoTask.Geo, Loca
 
         int HH = times / 3600;  //帰宅にかかる時間の時間部分抽出
         int mm = times % 3600 / 60;  //帰宅にかかる時間の分部分抽出
-        int ss = times % 60;  //帰宅にかかる時間の秒部分抽出
 
-        arriveTime(HH, mm, ss);  //自宅到着時刻の算出
+        arriveTime(HH, mm);  //自宅到着時刻の算出
 
         returnTime.setText("帰宅にかかる時間: " + (int) (min / 60) + " 時間 " + (int) (min % 60) + " 分");
         distance.setText("距離: " + dist + " キロメートル");
@@ -219,10 +191,11 @@ public class MainActivity extends AppCompatActivity implements GeoTask.Geo, Loca
         startActivity(intent);
     }
 
-    public void arriveTime(int addHour, int addMinute, int addSecond) {
+    //到着時刻算出
+    public void arriveTime(int addHour, int addMinute) {
         Calendar calendar = Calendar.getInstance();
 
-        SimpleDateFormat sdf = new SimpleDateFormat("HH時mm分ss秒");  //フォーマット初期化
+        SimpleDateFormat sdf = new SimpleDateFormat("HH時mm分");  //フォーマット初期化
 
         //現在時刻取得
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
@@ -230,10 +203,10 @@ public class MainActivity extends AppCompatActivity implements GeoTask.Geo, Loca
         int second = calendar.get(Calendar.SECOND);
 
         //現在時刻に帰宅にかかる時間を足す
-        calendar.set(hour, minute, second);
+        calendar.set(hour,minute,second);  //現在時刻取得
         calendar.add(Calendar.HOUR_OF_DAY, addHour);
         calendar.add(Calendar.MINUTE, addMinute);
-        calendar.add(Calendar.SECOND, addSecond);
+        //calendar.add(Calendar.SECOND, addSecond);
 
         Log.d("到着時刻", sdf.format(calendar.getTime()));
 
@@ -243,67 +216,27 @@ public class MainActivity extends AppCompatActivity implements GeoTask.Geo, Loca
 
     }
 
-    public void mailClick(View v) {
-        if (strTo == null) {
-            Toast.makeText(getApplicationContext(), "自宅設定を行ってください", Toast.LENGTH_SHORT).show();
-        } else {
-            sendMail();
+    public void mailClick(View view){
+        if(strTo==null){
+            Toast.makeText(getApplicationContext(),"自宅設定を行ってください",Toast.LENGTH_SHORT).show();
         }
-    }
+        else{
+            String mailTo = "";  //宛先メールアドレス
 
-    public void sendMail() {
-        try {
+            String subject = "帰宅連絡";  //件名
 
-            final Properties property = new Properties();
-            property.put("mail.smtp.host",                "smtp.gmail.com");
-            property.put("mail.host",                     "smtp.gmail.com");
-            property.put("mail.smtp.port",                "465");
-            property.put("mail.smtp.socketFactory.port",  "465");
-            property.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+            String mailText = times+"頃に帰宅します";  //メール本文
 
-            // セッション
-            final javax.mail.Session session = javax.mail.Session.getInstance(property, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("[送信元アカウント]", "[パスワード]");
-                }
-            });
+            //インテントのインスタンス生成
+            Intent intent = new Intent();
+            //インテントにアクション及び送信情報をセット
+            intent.setAction(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"+mailTo));
+            intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+            intent.putExtra(Intent.EXTRA_TEXT,mailText);
 
-            MimeMessage mimeMsg = new MimeMessage(session);
-
-            mimeMsg.setSubject("[メール件名]", "utf-8");
-            mimeMsg.setFrom(new InternetAddress("[送信元アカウント]@gmail.com"));
-            mimeMsg.setRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress("[送信先メールアドレス]"));
-
-            // 添付ファイル
-            final MimeBodyPart txtPart = new MimeBodyPart();
-            txtPart.setText("[メール本文]", "utf-8");
-
-            final MimeBodyPart filePart = new MimeBodyPart();
-            File file = new File("[添付ファイルパス]");
-            FileDataSource fds = new FileDataSource(file);
-            DataHandler data = new DataHandler(fds);
-            filePart.setDataHandler(data);
-            filePart.setFileName(MimeUtility.encodeWord("[メール本文の添付ファイル名]"));
-
-            final Multipart mp = new MimeMultipart();
-            mp.addBodyPart(txtPart);
-            mp.addBodyPart(filePart);
-            mimeMsg.setContent(mp);
-
-            // メール送信する。
-            final Transport transport = session.getTransport("smtp");
-            transport.connect("[送信元アカウント]", "[パスワード]");
-            transport.sendMessage(mimeMsg, mimeMsg.getAllRecipients());
-            transport.close();
-
-        } catch (MessagingException e) {
-
-        } catch (UnsupportedEncodingException e) {
-
-        } finally {
-
+            //メール起動
+            startActivity(intent);
         }
     }
 }
-
