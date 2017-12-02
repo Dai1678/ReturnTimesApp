4d264c1f659c187b0c3c8a91134a9b723c531939
Dai <dai7774@gmail.com>
2017-12-02 23:55:50 +0900

    profile トップ画面のToolbarボタン配置

--- app/src/main/java/org/t_robop/returntimesapp/SettingProfileActivity.java
+++ app/src/main/java/org/t_robop/returntimesapp/SettingProfileActivity.java
@@ -1,17 +1,23 @@
 package org.t_robop.returntimesapp;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class SettingProfileActivity extends AppCompatActivity {
+public class SettingProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,12 @@ public class SettingProfileActivity extends AppCompatActivity {
         //Toolbar
         Toolbar toolbar = (Toolbar)findViewById(R.id.profileToolbar);
         toolbar.setTitle("profile設定");   //TODO 意味考えて、適切な言葉に変更
+        setSupportActionBar(toolbar);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         //ListView
         ListView listView = (ListView)findViewById(R.id.profileList);
@@ -38,5 +50,29 @@ public class SettingProfileActivity extends AppCompatActivity {
         CustomProfileListAdapter adapter = new CustomProfileListAdapter(this,R.layout.profile_item,listItems);
         listView.setAdapter(adapter);
 
+    }
+
+    //ListViewクリック処理
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_profile,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }else if(item.getItemId() == R.id.save_profile){
+            Toast.makeText(SettingProfileActivity.this,"SAVED!",Toast.LENGTH_SHORT).show();
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
